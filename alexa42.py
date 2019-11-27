78d0b2bc0c8f6f99ab99323da02a5466bed454af
jordiprats <jordi.prats@gmail.com>
2019-11-27 22:25:28 +0100

    port

--- alexa42.py
+++ alexa42.py
@@ -9,4 +9,4 @@ def octoalert_intent():
     return alexandra.respond(ssml="<speak>42</speak>")
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', 9998, debug=True)
+    app.run('0.0.0.0', 9996, debug=True)
