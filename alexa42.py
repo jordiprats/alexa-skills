7af9eb65c571dfbe6d889e3a64303bca3eeab1ad
jordiprats <jordi.prats@gmail.com>
2019-11-28 07:48:17 +0100

    42

--- alexa42.py
+++ alexa42.py
@@ -6,9 +6,14 @@ app = alexandra.Application()
 def launch_handler(item):
     return alexandra.respond(ssml="<speak>42</speak>")
 
-@app.intent('rajoy')
+@app.intent('answer')
 def octoalert_intent():
     return alexandra.respond(ssml="<speak>42</speak>")
+
+@app.intent('respuesta')
+def octoalert_intent():
+    return alexandra.respond(ssml="<speak>42</speak>")
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', 9996, debug=True)
